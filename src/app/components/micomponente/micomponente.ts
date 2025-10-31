c344da3f2f41c5a19315249a1501c03f64af299c
Alejandraqt <alejandradurannq@gmail.com>
2025-10-30 20:36:44 -0500

    Cambios clase Angular

--- src/app/components/micomponente/micomponente.ts
+++ src/app/components/micomponente/micomponente.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AnimalService } from '../../services/animal-service';
 
 @Component({
   selector: 'app-micomponente',
@@ -9,6 +10,18 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class Micomponente {
-  titulo = "mi titulo"
+  
+  animalList: any = [];
+
+  constructor(private animalService: AnimalService) { }
+
+  getAllAnimals() {
+    this.animalService.getAllAnimalsData().subscribe((data: {}) => {
+      this.animalList = data;
+    });
+  }
+  ngOnInit() {
+    this.getAllAnimals();
+  }
 
 }
